db8b55478467c2af23a2e58f7c5890f63fe24493
caioteixeira <caio.ladeira@estudante.ufla.br>
2025-10-07 17:30:43 -0300

    divisaoMunicipal feita ate estado

--- src/main/java/org/example/paginaHome.java
+++ src/main/java/org/example/paginaHome.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class paginaHome {
-    public static void paginaHome(WebDriver driver) throws InterruptedException {
+    public static void paginaHome(WebDriver driver) {
 
         WebElement divisaoMunicipal = driver.findElement(By.id("router-link-divisao-municipal"));
         divisaoMunicipal.click();
