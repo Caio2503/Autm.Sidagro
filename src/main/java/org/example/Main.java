58b91df2c850fded7c0c64827300e32353bc1b09
caioteixeira <caio.ladeira@estudante.ufla.br>
2025-10-07 17:30:02 -0300

    divisaoMunicipal feita ate estado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import static org.example.paginaLogin.*;
 import static org.example.paginaHome.*;
