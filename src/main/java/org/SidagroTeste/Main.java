569465b9202c23d61057a05fc9cfb4474d842d21
caioteixeira <caio.ladeira@estudante.ufla.br>
2025-10-16 20:40:38 -0300

    Versão final

--- src/main/java/org/SidagroTeste/Main.java
+++ src/main/java/org/SidagroTeste/Main.java
@@ -5,20 +5,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
-import static org.SidagroTeste.paginaLogin.*;
-import static org.SidagroTeste.divisaoMunicipal.*;
-
 
 public class Main {
     public static void main(String[] args) { // exception usado para dar uma "pausa" no codigo
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); // tempo maximo para uma acao, mantido alto por possiveis instabilidades com a rede
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5)); // mesma coisa do de cima, mas para os driver
-
         driver.get("https://sidagro-homolog.dac.ufla.br/");
-        paginaLogin(wait);
-        divisaoMunicipal(driver, wait);
+        String cpf = "104.874.126-56";
+        String senha = "admin";
+        String nomeDivisao = "Teste51";
+        String nomeEditado = "Teste50 Editado";
+        String municipio = "Belo Horizonte";
+        String estado = "Minas Gerais";
+        String latitude = String.valueOf(-19.97851);
+        String longitude = String.valueOf(-43.96912);
+        boolean distrito = false;//variavel para definir se será selecionado distrito ou localidade no campo "tipo"
+
+
+        paginaLogin loginPage = new paginaLogin(wait);
+        loginPage.executarLogin(cpf, senha);
+        divisaoMunicipal divisaoPage = new divisaoMunicipal(driver, wait);
+        divisaoPage.preencherDivisaoMunicipal(nomeDivisao, nomeEditado, distrito, municipio, estado, latitude, longitude);
         driver.quit();
     }
-
 }
--- src/main/java/org/SidagroTeste/paginaLogin.java
+++ src/main/java/org/SidagroTeste/paginaLogin.java
@@ -1,29 +1,41 @@
 package org.SidagroTeste;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 public class paginaLogin {
-    public static void paginaLogin(WebDriverWait wait) {
+    private WebDriverWait wait;
 
+    public paginaLogin(WebDriverWait wait) {
+        this.wait = wait;
+    }
+
+    public void executarLogin(String cpf, String senha) {
         WebElement campoCpf = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("cpf-input")));
-        campoCpf.sendKeys("104.874.126-56"); //pontuacao opcional
+        campoCpf.sendKeys(cpf);
 
         WebElement campoSenha = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("senha-input")));
-        campoSenha.sendKeys("admin");
+        campoSenha.sendKeys(senha);
 
         WebElement botaoEntrar = wait.until(ExpectedConditions.elementToBeClickable(By.id("entrar-button")));
         botaoEntrar.click();
 
-        paginaHome(wait);
+        paginaHome();
     }
 
-    public static void paginaHome(WebDriverWait wait) {
-        WebElement divisaoMunicipal = wait.until(ExpectedConditions.elementToBeClickable(By.id("router-link-divisao-municipal")));
-        divisaoMunicipal.click();
+    public void paginaHome() {
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(
+                    By.xpath("//*[contains(text(), 'Cadastros')]")
+            ));
+            System.out.println("Login completo");
+            WebElement divisaoMunicipal = wait.until(ExpectedConditions.elementToBeClickable(By.id("router-link-divisao-municipal")));
+            divisaoMunicipal.click();
+        } catch (TimeoutException e) {
+            System.out.println("Falha ao fazer login. O elemento de texto 'Cadastros' nao foi encontrado.");
+        }
     }
-
 }
